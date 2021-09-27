4918c54fcecff47f92f1eeeeabf75f06031eafaf
jj77847 <81172135+jj77847@users.noreply.github.com>
2021-09-27 21:26:23 +0100

    layout

---
+++ extra-content/03-Javascript/vowelsCounter/script.js
@@ -0,0 +1,22 @@
+// Declare Sentence
+
+const sentence = "Javascript ain't so bad!!";
+
+// convert string to lowercase and then to an array
+
+const sentenceArray = sentence.split("");
+
+// Declare Vowels
+const VowelsArray = ['a', 'e', 'i', 'o', 'u']
+
+// Declare Vowels Count
+le VowelsCount = 0
+
+// Loop threw the Sentence
+for (let i=0; i<sentenceArray.length; i++) {
+    // Get Character
+
+    // 
+}
+// Display Vowels Count
+console.log('There are ' + VowelsCount + ' vowels in that sentence.')
