7f0adf4e96553508c3f8df4b4a5fb6ec0bc714ac
jj77847 <81172135+jj77847@users.noreply.github.com>
2021-09-27 21:32:22 +0100

    lay

--- extra-content/03-Javascript/vowelsCounter/script.js
+++ extra-content/03-Javascript/vowelsCounter/script.js
@@ -13,10 +13,20 @@ const VowelsArray = ['a', 'e', 'i', 'o', 'u']
 le VowelsCount = 0
 
 // Loop threw the Sentence
+
 for (let i=0; i<sentenceArray.length; i++) {
+
     // Get Character
 
-    // 
-}
+    const character = sentenceArray[i]
+    // Check if character is vowel
+
+    if (VowelsArray.includes(character)) {
+        
+    }
+    // if Yes increment VowelCount
+    VowelsCount+= 1;
+}   
+
 // Display Vowels Count
 console.log('There are ' + VowelsCount + ' vowels in that sentence.')
