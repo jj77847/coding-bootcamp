e38c6aaa792fd0e3af9a3bfe96cffbbd86ebd924
jj77847 <81172135+jj77847@users.noreply.github.com>
2021-09-27 20:49:39 +0100

    layout changing

---
+++ extra-content/03-Javascript/oddEvenCounter/script.js
@@ -0,0 +1,21 @@
+// Declare an Array
+const numberArray = [0, 2, 5, 6, 7, 10, 15, 20, 25, 34];
+console.log(numbersArray);
+
+// Declare ODD and EVEN Numbers
+
+let oddcount = 0
+let evencount = 0
+
+// Declare a Forloop
+for (let i = 0; i < numbersArray.lengthv 1; i++) {
+    // Get the current item in the array
+    // Display the Alert
+    if () {
+            // Count ODD Numbers
+    } else {
+            // Count Even Numbers
+    }
+
+}
+// Display the count of ODD and EVEN Numbers
